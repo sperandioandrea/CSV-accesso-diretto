a27f3ef0275fe3175ea8c583f891b17393f49529
andrea <sperandio.andrea.studente@itispaleocapa.it>
2023-09-08 16:02:38 +0200

    Creazione libreria, struct e collegamento file

--- CSV-accesso diretto-Form/Form1.cs
+++ CSV-accesso diretto-Form/Form1.cs
@@ -11,13 +11,26 @@ using System.Windows.Forms;
 
 namespace CSV_accesso_diretto_Form
 {
- 
+
+    public struct classe
+    {
+        public string Comune;
+        public string Provincia;
+        public string Regione;
+    }
     public partial class Form1 : Form
     {
-       
+        Funzioni f;
+        public classe[] p;
+        public int dim;
+        public string nomefile;
         public Form1()
         {
-           
+            InitializeComponent();
+            f = new Funzioni();
+            p = new classe[100];
+            dim = 0;
+            nomefile = @"";
         }
 
         private void Form1_Load(object sender, EventArgs e)
