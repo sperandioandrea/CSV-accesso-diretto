2e9fa5e8987c5d0c38082f235d17a65af0b9f27b
andrea <sperandio.andrea.studente@itispaleocapa.it>
2023-09-08 16:00:28 +0200

    Aggiungere i file di progetto.

---
+++ CSV-accesso diretto-Form/Form1.cs
@@ -0,0 +1,28 @@
+﻿using CSV_accesso_diretto_Form.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_accesso_diretto_Form
+{
+ 
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
